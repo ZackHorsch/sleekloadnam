be984db2d858b1536dbd30dabe3871dc999ad60c
ZackHorsch <40706892+ZackHorsch@users.noreply.github.com>
2020-04-03 20:22:53 -0500

    addedstuff
    gg

---
+++ js/config.js
@@ -0,0 +1,184 @@
+//       _           _    _                     _ 
+//      | |         | |  | |                   | |
+//   ___| | ___  ___| | _| |     ___   __ _  __| |
+//  / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+//  \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+//  |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+//     version 2.16
+
+// URL for the server logo
+// Leave it blank to use text instead
+// (Put the files in the images folder)
+// (You can also use external URL)
+var l_serverImage = "ohnoback.png";
+
+// Center logo?
+// 'true' or 'false'
+var l_centerLogo = false;
+
+// Display current map and gamemode?
+// 'true' or 'false'
+var l_displayMapGamemode = false;
+
+// Custom server name configuration
+// Leave it blank for automatic server name
+var l_serverName = " OH | Vietnam War";
+
+// Use a video for the background?
+// Otherwise it will use images
+// 'true' or 'false'
+var l_bgVideo = false;
+
+// Default available videos:
+// "aurora.webm"
+// "bluefall.webm"
+// "bluetilefloor.webm"
+// "bluewhitecircles.webm"
+// "bokehlens.webm"
+// "cleanbokeh.webm"
+// "coldnight.webm"
+// "colorfulburst.webm"
+// "dancingstars.webm"
+// "dropsflares.webm"
+// "focusorangebokeh.webm"
+// "glimmering.webm"
+// "glowingrings.webm"
+// "greencircles.webm"
+// "hues.webm"
+// "inthedeep.webm"
+// "modernflourish.webm"
+// "orangeaura.webm"
+// "warminside.webm"
+// "yellowgreen.webm"
+//
+// You can add your own video backgrounds
+// Just make sure they are using webm VP8 format 
+// (Place the videos in the backgrounds/videos folder)
+// (You can also use external URL)
+var l_background = "";
+
+// (Image-background only)
+// Enable map-based background image?
+// If enabled, the background will be an image based on the server current map
+// You need to have a background image with the same name as the map
+// Also, the image needs to be in .jpg format
+// E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
+// 'true' or 'false'
+var l_bgImageMapBased = false;
+
+// (Image-background only)
+// Place one or more images
+// Multiple images will automatically shuffle between them
+// (Place the files in the backgrounds/images folder)
+// (You can also use external URL)
+var l_bgImages = [
+	"p1.jpg",
+	"p2.jpg",
+	"p3.jpg",
+	"p4.jpg",
+	"p5.jpg",
+	"p6.jpg",
+	"p7.jpg",
+	"p8.jpg",
+	"p9.jpg",
+	"p10.jpg",
+];
+
+// (Image-background only)
+// Random background images order?
+// 'true' or 'false'
+var l_bgImagesRandom = true;
+
+// (Image-background only)
+// Delay between background images changes
+// in milliseconds
+var l_bgImageDuration = 5000;
+
+// (Image-background only)
+// Image background fade velocity
+// in milliseconds
+var l_bgImageFadeVelocity = 2000;
+
+// Enable background overlay?
+// 'true' or 'false'
+var l_bgOverlay = true;
+
+// Background darkening amount
+// 0 for none (0%), 100 for pitch black (100%)
+var l_bgDarkening = 0;
+
+// Play music during the loading screen?
+// 'true' or 'false'
+var l_music = true;
+
+// Display current music name?
+// 'true' or 'false'
+var l_musicDisplay = false;
+
+// Music playlist
+// Add as many youtube ids/urls or ogg files you want
+// (Place the ogg files in the music folder)
+// (You can also use external URL for the ogg files)
+var l_musicPlaylist = [
+	{ogg: "u1.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "u2.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "u3.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "u4.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "u5.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "u6.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "u7.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "u8.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "u9.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "v1.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "v2.ogg", name: "Lullaby of Deserted Hell"}
+];
+
+// Random music order?
+// 'true' or 'false'
+var l_musicRandom = true;
+
+// Music volume
+// Choose a value between 0 (quietest) and 100 (loudest)
+var l_musicVolume = 10;
+
+// Enable custom messages?
+// 'true' or 'false'
+var l_messagesEnabled = false;
+
+// Enter your custom messages below
+var l_messages = [
+	"Do not Troll",
+	"Please read the rules, type !motd",
+	"Always obey the officer, and if you have complaints, please contact staff.",
+	"If you see errors, download the content from the link in chat.",
+	"To contact staff, please type @ (msg) and staff will arrive shortly.",
+	"Do not forget to get trained if you are a recruit",
+	"the radio command is /r",
+	"OH Discord Discord Invite Code:Ss7at4v",
+];
+
+// Random message order?
+// 'true' or 'false'
+var l_messagesRandom = true;
+
+// Delay between message changes
+// in milliseconds
+var l_messagesDelay = 5000;
+
+// Messages fade time
+// in milliseconds
+var l_messagesFade = 1000;
+
+// Show errors inside Garry's Mod?
+// Can be useful for finding errors
+// 'true' or 'false'
+var l_showErrorsIngame = true;
+
+//==============================================================
+//========================== WARNING ===========================
+//==============================================================
+//============ Do not edit anything below this line ============
+//==============================================================
+var checkConfigFile = function() {
+	return true;
+};
